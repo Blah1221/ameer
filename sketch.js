d2721d8a4cdbf925002f9c239994ccba43baf634
Blah1221 <107046491+Blah1221@users.noreply.github.com>
2022-06-07 16:24:37 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,50,10)
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(RIGHT_ARROW)){
+    box.x = box.x+5
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.x = box.x-5
+  }
+  drawSprites()
+
+}
+
+
+
+
